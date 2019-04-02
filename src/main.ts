9b422ba35ddf1e74125953af89da918c4d6d2e93
Brayan <brayan_103@outlook.com>
2019-04-02 15:15:01 -0500

    adsf

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,48 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./styles/quasar.styl";
+import "@quasar/extras/material-icons/material-icons.css";
+import {
+  Quasar,
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+  QParallax
+} from "quasar";
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+    QParallax
+  },
+  directives: {},
+  plugins: {}
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  
+  <div class="q-pa-md">
+     <br>
+    <br>
+    <br>
+
+    <q-parallax :height="150">
+      <template v-slot:media>
+        <video width="720" height="440" poster="https://cdn.quasar-framework.org/img/polina.jpg" autoplay loop muted>
+          <source type="video/webm" src="https://cdn.quasar-framework.org/img/polina.webm">
+          <source type="video/mp4" src="https://cdn.quasar-framework.org/img/polina.mp4">
+        </video>
+      </template>
+
+      <h3 class="text-white">Video</h3>
+    </q-parallax>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
   </div>
 </template>
+<script>
+export default {
+  name: "PageAbout"
+};
+</script>
